f4ec7cddfd8571ea2abfef5249d8fe9c53b2617d
kaylawallace <37516604+kaylawallace@users.noreply.github.com>
2019-01-30 13:34:35 +0000

    simple grid

--- GridGame/Form1.cs
+++ GridGame/Form1.cs
@@ -12,16 +12,27 @@ namespace GridGame
 {
     public partial class GridGame : Form
     {
+        Button[,] btn = new Button[5, 5];
+        
         public GridGame()
         {
             InitializeComponent();
-
-            Button[,] btnGrid = new Button[4,4];
-
-            foreach (Button tmpBtn in btnGrid)
+            for (int x = 0; x < btn.GetLength(0); x++)
             {
+                for (int y = 0; y<btn.GetLength(1); y++)
+                {
+                    btn[x, y] = new Button();
+                    btn[x, y].SetBounds(55 * x, 55 * y, 45, 45);
+                    btn[x, y].Text = Convert.ToString((x + 1) + "," + (y + 1));
+                    btn[x, y].Click += new EventHandler(this.BtnEvent_Click);
+                    Controls.Add(btn[x, y]);
+                }
             }
-
+        }
+        
+        void BtnEvent_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(((Button)sender).Text);
         }
 
         private void exitBtn_Click(object sender, EventArgs e)
