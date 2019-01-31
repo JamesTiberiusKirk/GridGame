ccb25e1268334dc0564f0945212fade90434c609
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2019-01-31 21:30:57 +0000

    Kayla's Commit
    Checked off Colours, number of lives, scores in the top right corner, chosing random button to change, etc.

--- GridGame/Form1.Designer.cs
+++ GridGame/Form1.Designer.cs
@@ -28,49 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.exitBtn = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.LblLivesTitle = new System.Windows.Forms.Label();
+            this.LblLivesCounter = new System.Windows.Forms.Label();
+            this.LblScoreTitle = new System.Windows.Forms.Label();
+            this.LblScoreCounter = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // exitBtn
+            // BtnExit
             // 
-            this.exitBtn.Location = new System.Drawing.Point(32, 592);
-            this.exitBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.exitBtn.Name = "exitBtn";
-            this.exitBtn.Size = new System.Drawing.Size(112, 35);
-            this.exitBtn.TabIndex = 0;
-            this.exitBtn.Text = "Exit";
-            this.exitBtn.UseVisualStyleBackColor = true;
-            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            this.BtnExit.Font = new System.Drawing.Font("Copperplate Gothic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnExit.Location = new System.Drawing.Point(12, 399);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(88, 39);
+            this.BtnExit.TabIndex = 0;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
             // 
-            // button1
+            // LblLivesTitle
             // 
-            this.button1.Location = new System.Drawing.Point(208, 592);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 35);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.LblLivesTitle.AutoSize = true;
+            this.LblLivesTitle.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLivesTitle.Location = new System.Drawing.Point(517, 9);
+            this.LblLivesTitle.Name = "LblLivesTitle";
+            this.LblLivesTitle.Size = new System.Drawing.Size(70, 19);
+            this.LblLivesTitle.TabIndex = 1;
+            this.LblLivesTitle.Text = "LIVES:";
+            // 
+            // LblLivesCounter
+            // 
+            this.LblLivesCounter.AutoSize = true;
+            this.LblLivesCounter.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLivesCounter.Location = new System.Drawing.Point(593, 9);
+            this.LblLivesCounter.Name = "LblLivesCounter";
+            this.LblLivesCounter.Size = new System.Drawing.Size(21, 19);
+            this.LblLivesCounter.TabIndex = 2;
+            this.LblLivesCounter.Text = "3";
+            // 
+            // LblScoreTitle
+            // 
+            this.LblScoreTitle.AutoSize = true;
+            this.LblScoreTitle.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblScoreTitle.Location = new System.Drawing.Point(632, 9);
+            this.LblScoreTitle.Name = "LblScoreTitle";
+            this.LblScoreTitle.Size = new System.Drawing.Size(82, 19);
+            this.LblScoreTitle.TabIndex = 3;
+            this.LblScoreTitle.Text = "SCORE:";
+            // 
+            // LblScoreCounter
+            // 
+            this.LblScoreCounter.AutoSize = true;
+            this.LblScoreCounter.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblScoreCounter.Location = new System.Drawing.Point(720, 9);
+            this.LblScoreCounter.Name = "LblScoreCounter";
+            this.LblScoreCounter.Size = new System.Drawing.Size(21, 19);
+            this.LblScoreCounter.TabIndex = 4;
+            this.LblScoreCounter.Text = "0";
             // 
             // GridGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.exitBtn);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblScoreCounter);
+            this.Controls.Add(this.LblScoreTitle);
+            this.Controls.Add(this.LblLivesCounter);
+            this.Controls.Add(this.LblLivesTitle);
+            this.Controls.Add(this.BtnExit);
             this.Name = "GridGame";
             this.Text = "GridGame";
             this.Load += new System.EventHandler(this.GridGame_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button exitBtn;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Label LblLivesTitle;
+        private System.Windows.Forms.Label LblLivesCounter;
+        private System.Windows.Forms.Label LblScoreTitle;
+        private System.Windows.Forms.Label LblScoreCounter;
     }
 }
 
--- GridGame/Form1.cs
+++ GridGame/Form1.cs
@@ -13,6 +13,8 @@ namespace GridGame
     public partial class GridGame : Form
     {
         Button[,] btn = new Button[5, 5];
+        int lives = 3;
+        int score = 0;
         
         public GridGame()
         {
@@ -21,33 +23,75 @@ namespace GridGame
         
         void LoadGrid()
         {
+            Color[] colors = new Color[4];
+            Random rnd = new Random();
+
+            colors[0] = Color.Goldenrod;
+            colors[1] = Color.MediumOrchid;
+            colors[2] = Color.CornflowerBlue;
+            colors[3] = Color.Coral;
+
             for (int x = 0; x < btn.GetLength(0); x++)
             {
                 for (int y = 0; y<btn.GetLength(1); y++)
                 {
                     btn[x, y] = new Button();
-                    btn[x, y].SetBounds(55 * x, 55 * y, 45, 45);
-                    btn[x, y].Text = Convert.ToString((x + 1) + "," + (y + 1));
+                    btn[x, y].SetBounds(50 * x, 50 * y, 50, 50);
+                    btn[x, y].BackColor = colors[rnd.Next(0, 4)];
+                    //btn[x, y].Text = Convert.ToString((x + 1) + "," + (y + 1));
                     btn[x, y].Click += new EventHandler(this.BtnEvent_Click);
                     Controls.Add(btn[x, y]);
                 }
             }
+        }
 
+        void RandButton()
+        {
+            int x = 0;
+            int y = 0;
+            Random randX = new Random();
+            Random randY = new Random();
+
+            x = randX.Next(1, btn.GetLength(0));
+            y = randY.Next(1, btn.GetLength(1));
+
+            btn[x, y].BackColor = Color.Black;
+
+            btn[x, y].Click += new EventHandler(this.RandButton_Click);
         }
 
         void BtnEvent_Click(object sender, EventArgs e)
         {
-            Console.WriteLine(((Button)sender).Text);
+            ((Button)sender).BackColor = Color.Red;
+            lives--;
+            LblLivesCounter.Text = lives.ToString();
+
+            if (lives == 0)
+            {
+                MessageBox.Show("GAME OVER");
+                Close();
+            }
         }
 
-        private void exitBtn_Click(object sender, EventArgs e)
+        void RandButton_Click(object sender, EventArgs e)
         {
-            
+            ((Button)sender).BackColor = Color.LightGreen;
+            score++;
+            lives++;
+            LblScoreCounter.Text = score.ToString();
+            LblLivesCounter.Text = lives.ToString();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
         }
 
         private void GridGame_Load(object sender, EventArgs e)
         {
-
+            LoadGrid();
+            RandButton();
         }
+
     }
 }
