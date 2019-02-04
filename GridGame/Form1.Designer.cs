21246925a3476932e20e2ed95beed5cb4953b06b
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2019-02-04 09:46:45 +0000

    Some upgrades.
    TBH this is what i forgot to commit last time so I dont remmeber what the changes are :(.

--- GridGame/Form1.Designer.cs
+++ GridGame/Form1.Designer.cs
@@ -33,14 +33,22 @@
             this.LblLivesCounter = new System.Windows.Forms.Label();
             this.LblScoreTitle = new System.Windows.Forms.Label();
             this.LblScoreCounter = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BtnExit
             // 
             this.BtnExit.Font = new System.Drawing.Font("Copperplate Gothic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnExit.Location = new System.Drawing.Point(12, 399);
+            this.BtnExit.Location = new System.Drawing.Point(18, 614);
+            this.BtnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BtnExit.Name = "BtnExit";
-            this.BtnExit.Size = new System.Drawing.Size(88, 39);
+            this.BtnExit.Size = new System.Drawing.Size(132, 60);
             this.BtnExit.TabIndex = 0;
             this.BtnExit.Text = "Exit";
             this.BtnExit.UseVisualStyleBackColor = true;
@@ -50,9 +58,10 @@
             // 
             this.LblLivesTitle.AutoSize = true;
             this.LblLivesTitle.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblLivesTitle.Location = new System.Drawing.Point(517, 9);
+            this.LblLivesTitle.Location = new System.Drawing.Point(780, 41);
+            this.LblLivesTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LblLivesTitle.Name = "LblLivesTitle";
-            this.LblLivesTitle.Size = new System.Drawing.Size(70, 19);
+            this.LblLivesTitle.Size = new System.Drawing.Size(107, 29);
             this.LblLivesTitle.TabIndex = 1;
             this.LblLivesTitle.Text = "LIVES:";
             // 
@@ -60,9 +69,10 @@
             // 
             this.LblLivesCounter.AutoSize = true;
             this.LblLivesCounter.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblLivesCounter.Location = new System.Drawing.Point(593, 9);
+            this.LblLivesCounter.Location = new System.Drawing.Point(894, 41);
+            this.LblLivesCounter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LblLivesCounter.Name = "LblLivesCounter";
-            this.LblLivesCounter.Size = new System.Drawing.Size(21, 19);
+            this.LblLivesCounter.Size = new System.Drawing.Size(32, 29);
             this.LblLivesCounter.TabIndex = 2;
             this.LblLivesCounter.Text = "3";
             // 
@@ -70,9 +80,10 @@
             // 
             this.LblScoreTitle.AutoSize = true;
             this.LblScoreTitle.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblScoreTitle.Location = new System.Drawing.Point(632, 9);
+            this.LblScoreTitle.Location = new System.Drawing.Point(952, 41);
+            this.LblScoreTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LblScoreTitle.Name = "LblScoreTitle";
-            this.LblScoreTitle.Size = new System.Drawing.Size(82, 19);
+            this.LblScoreTitle.Size = new System.Drawing.Size(125, 29);
             this.LblScoreTitle.TabIndex = 3;
             this.LblScoreTitle.Text = "SCORE:";
             // 
@@ -80,25 +91,81 @@
             // 
             this.LblScoreCounter.AutoSize = true;
             this.LblScoreCounter.Font = new System.Drawing.Font("Copperplate Gothic Bold", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblScoreCounter.Location = new System.Drawing.Point(720, 9);
+            this.LblScoreCounter.Location = new System.Drawing.Point(1084, 41);
+            this.LblScoreCounter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LblScoreCounter.Name = "LblScoreCounter";
-            this.LblScoreCounter.Size = new System.Drawing.Size(21, 19);
+            this.LblScoreCounter.Size = new System.Drawing.Size(32, 29);
             this.LblScoreCounter.TabIndex = 4;
             this.LblScoreCounter.Text = "0";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1200, 33);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(70, 29);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.mediumToolStripMenuItem});
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.startToolStripMenuItem.Text = "Start";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.easyToolStripMenuItem.Text = "Easy";
+            this.easyToolStripMenuItem.Click += new System.EventHandler(this.LoadGrid);
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            this.mediumToolStripMenuItem.Click += new System.EventHandler(this.LoadGrid);
+            // 
             // GridGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.LblScoreCounter);
             this.Controls.Add(this.LblScoreTitle);
             this.Controls.Add(this.LblLivesCounter);
             this.Controls.Add(this.LblLivesTitle);
             this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "GridGame";
             this.Text = "GridGame";
             this.Load += new System.EventHandler(this.GridGame_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,6 +178,12 @@
         private System.Windows.Forms.Label LblLivesCounter;
         private System.Windows.Forms.Label LblScoreTitle;
         private System.Windows.Forms.Label LblScoreCounter;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
     }
 }
 
--- GridGame/Form1.cs
+++ GridGame/Form1.cs
@@ -12,37 +12,65 @@ namespace GridGame
 {
     public partial class GridGame : Form
     {
-        Button[,] btn = new Button[5, 5];
-        int lives = 3;
-        int score = 0;
+
+        private Button[,] btn;
+        private int lives = 3;
+        private int score = 0;
         
         public GridGame()
         {
             InitializeComponent();
         }
-        
-        void LoadGrid()
+       
+        void LoadGrid(object sender, EventArgs e)
         {
-            Color[] colors = new Color[4];
-            Random rnd = new Random();
-
-            colors[0] = Color.Goldenrod;
-            colors[1] = Color.MediumOrchid;
-            colors[2] = Color.CornflowerBlue;
-            colors[3] = Color.Coral;
-
             for (int x = 0; x < btn.GetLength(0); x++)
             {
-                for (int y = 0; y<btn.GetLength(1); y++)
+                for (int y = 0; y < btn.GetLength(1); y++)
                 {
                     btn[x, y] = new Button();
                     btn[x, y].SetBounds(50 * x, 50 * y, 50, 50);
-                    btn[x, y].BackColor = colors[rnd.Next(0, 4)];
                     //btn[x, y].Text = Convert.ToString((x + 1) + "," + (y + 1));
                     btn[x, y].Click += new EventHandler(this.BtnEvent_Click);
                     Controls.Add(btn[x, y]);
                 }
             }
+
+            Random rnd = new Random();
+            Button b = (Button)sender;
+            switch (b.Text) {
+                case "Easy":
+                    btn = new Button[4, 4];
+                    //Colours
+                    Color[] colours = new Color[4];
+
+                    colours[0] = Color.Goldenrod;
+                    colours[1] = Color.MediumOrchid;
+                    colours[2] = Color.CornflowerBlue;
+                    colours[3] = Color.Coral;
+                    for (int x = 0; x < btn.GetLength(0); x++)
+                    {
+                        for (int y = 0; y<btn.GetLength(1); y++)
+                        {
+                            btn[x, y].BackColor = colours[rnd.Next(0, 4)];
+                        }
+                    }
+                    break;
+                case "Medium":
+                    btn = new Button[6, 6];
+                    //Images
+                    break;
+                case "Expert":
+                    btn = new Button[10, 10];
+                    //Numbers
+                    break;
+                default:
+                    //should not run
+                    Console.WriteLine("somehow nothing was passed");
+                    break;
+            }
+            
+            
         }
 
         void RandButton()
@@ -93,5 +121,9 @@ namespace GridGame
             RandButton();
         }
 
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
